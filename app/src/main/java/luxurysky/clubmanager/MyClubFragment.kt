6576172ccba28280b22a80e2ebcd10408b31efbf
HWANGJIN <sky3100>
2017-12-18 11:34:45 +0900

    - main화면 레이아웃 변경 - realm 라이브러리 추가

--- app/src/main/java/luxurysky/clubmanager/MyClubFragment.kt
+++ app/src/main/java/luxurysky/clubmanager/MyClubFragment.kt
@@ -6,13 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-/**
- * A placeholder fragment containing a simple view.
- */
-class MainActivityFragment : Fragment() {
+class MyClubFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return null
+//        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 }
